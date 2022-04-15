d21f41b0d07cdbee2ea142e27cfb0fd1272adbbb
Alex Cattell <abcattell91@gmail.com>
2022-04-15 21:34:47 +0100

    testing apis

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
+require "open-uri"
+
 class PagesController < ApplicationController
 
   def home
+  end
+
+  def trending
+    response = URI.open("https://imdb-api.com/en/API/ComingSoon/k_k3s70pl3")
+    json = JSON.parse(response)
+    json['title']
   end
 
 end
