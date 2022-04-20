f685d3f8029ff0bef0b0b9fec36fafd15d2ddccc
Alex Cattell <abcattell91@gmail.com>
2022-04-20 12:02:41 +0100

    finished codeacademy task

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,4 @@ class PagesController < ApplicationController
   def home
   end
 
-  def trending
-    response = URI.open("https://imdb-api.com/en/API/ComingSoon/k_k3s70pl3")
-    json = JSON.parse(response)
-    json['title']
-  end
-
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-require("../includes/api_parse")
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
